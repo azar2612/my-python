0a38e79c3004eddaa98978f294063d79aae31992
MOHAMED AZARUDEEN M <asarudeen26121997@gmail.com>
2020-02-20 11:42:17 +0530

    To find a GCD of two numbers using python

---
+++ GCD_finder_using_python.py
@@ -0,0 +1,10 @@
+def hcfnaive(a,b): 
+    if(b==0): 
+        return a 
+    else: 
+        return hcfnaive(b,a%b) 
+a = 60
+b= 48
+
+print ("The gcd of 60 and 48 is : ",end = "") 
+print (hcfnaive(60,48)) 
